2f98ec415d4883c2a796e61b2417ae241147f42c
Siang Lee, Khaw <khawslee@gmail.com>
2025-01-15 15:39:03 +0800

    Add Common Interview Questions

--- app.py
+++ app.py
@@ -55,7 +55,7 @@ job_description = st.text_area("Paste the job description here")
 uploaded_file = st.file_uploader("Upload your resume (PDF)", type="pdf")
 
 st.sidebar.title("Navigation")
-page = st.sidebar.radio("Go to", ["Rephase to Google XYZ", "Core competencies extractor", "Bullet Point Rephaser", "ATS Friendly Resume Generator", "Generate Cover Letter", "Introduction Generator", "Top Interview Questions"])
+page = st.sidebar.radio("Go to", ["Rephase to Google XYZ", "Core competencies extractor", "Bullet Point Rephaser", "ATS Friendly Resume Generator", "Generate Cover Letter", "Introduction Generator", "Top Interview Questions", "Common Interview Questions"])
 
 if page == "Rephase to Google XYZ":
     st.title("📄 Rephase to Google Recruiter XYZ formula")
@@ -355,7 +355,45 @@ elif page == "Top Interview Questions":
                     Guidelines:
                     - The questions should be relevant to the job description and the candidate's resume.
                     - The answers should be concise, professional, and highlight the candidate's strengths and qualifications, utilizing CARL method Context, Action, Result, Learning.
-                    - Each answer should be no more 5 minutes.
+                    - Each answer should be no more 2 minutes.
+                    - The output should be in a clear and easy-to-read format.
+                    
+                    Inputs:
+                    Job Description: {job_description}
+                    Resume: {resume_text}
+                    
+                    Output:
+                    Provide the questions and answers in the following format:
+                    Question: [Question]
+                    Answer: [Answer]
+                    """
+                response = model.generate_content(prompt)
+                st.write(response.text)
+        else:
+            st.warning("Please enter job description, resume, API key and select a model.")
+            
+elif page == "Common Interview Questions":
+    st.title("Common Interview Questions")
+    
+    #List of questions
+    listofQuestion = ["What makes you unique?", "Tell me about yourself and your qualifications.", "Why do you want to work at this company?", "What interests you about this role?", "What motivates you?", "What are your greatest strengths?", "What are your greatest weaknesses?", "What are your goals for the future?", "Where do you think you'll be in five years?", "What did you like most about your last position?", "What did you like least about your last position?", "Can you tell me about a difficult work situation and how you overcame it?", "How do you respond to stress or change?", "How do you handle conflict at work?", "What is your greatest accomplishment?", "How do you define success?", "How do your skills align with this role?", "Why should we hire you?", "Why are you leaving your current job?", "What is your salary range expectation?", "Do you have any questions?", "What are you passionate about?", "What is your teaching philosophy?", "What does customer service mean to you?", "Tell me about your work experience.", "How do you work under pressure?", "What is your dream job?", "What can you bring to the company?"]
+    
+    if st.button("Generate Questions and Answers"):
+        if job_description and model_name and api_key and uploaded_file:
+            with st.spinner("Generating..."):
+                genai.configure(api_key=api_key)
+                model = genai.GenerativeModel(model_name)
+                resume_text = extract_text_from_pdf(uploaded_file)
+                prompt = f"""
+                    You are an expert career assistant specializing in crafting compelling interview questions and answers.
+                    
+                    Task:
+                    - Provide a concise and professional answer for each question listed in {listofQuestion}, tailored to the job description and resume.
+                    
+                    Guidelines:
+                    - The questions should be relevant to the job description and the candidate's resume.
+                    - The answers should be concise, professional, and highlight the candidate's strengths and qualifications, utilizing CARL method Context, Action, Result, Learning.
+                    - Each answer should be no more 2 minutes.
                     - The output should be in a clear and easy-to-read format.
                     
                     Inputs:
